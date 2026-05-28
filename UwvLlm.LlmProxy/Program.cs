bae7132b9e6400d6fd18db9745fb5577943b0611
Willem Beltman <hwj.beltman@gmail.com>
2026-05-28 11:35:53 +0200

    Netjes hoor codex

--- UwvLlm.LlmProxy/Program.cs
+++ UwvLlm.LlmProxy/Program.cs
@@ -1,6 +1,8 @@
 ﻿using gAPI.Core.Extensions;
+using gAPI.Core.Ids;
 using gAPI.Core.Interfaces;
 using gAPI.Core.Server;
+using gAPI.Core.Server.Collections;
 using gAPI.Core.Server.Extensions;
 using gAPI.Core.Server.Mappings;
 using gAPI.Core.ServiceBus.Extensions;
@@ -9,12 +11,14 @@ using gAPI.Core.ServiceBus.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using UwvLlm.Api.Extensions;
+using UwvLlm.Infrastructure.Data.CrudServices;
 using UwvLlm.Infrastructure.Data.Mappings;
 using UwvLlm.Infrastructure.Llm.Clients;
 using UwvLlm.Infrastructure.Llm.Interfaces;
 using UwvLlm.LlmProxy.Core.Handlers;
 using UwvLlm.LlmProxy.Extensions;
 using UwvLlm.Shared.Public;
+using UwvLlm.Shared.Public.CrudInterfaces;
 using UwvLlm.Shared.Public.Dtos;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -27,8 +31,12 @@ builder.Services.AddAuthenticationServices<UwvLlm.Infrastructure.Data.Entities.U
 builder.Services.AddScoped<IStateMapping<UwvLlm.Infrastructure.Data.Entities.User, State>, StateMapping>();
 builder.Services.AddScoped<IStateUserMapping<UwvLlm.Infrastructure.Data.Entities.User, StateUser>, StateUserMapping>();
 builder.Services.AddScoped<IStateParser<State>, StateParser>();
-builder.Services.AddCrudMappings();
-builder.Services.AddCrudUseCases();
+
+builder.Services.AddSingleton<WssSessionCache>();
+builder.Services.AddCrudMappings(); // Wordt fysiek gegenereerd
+builder.Services.AddCrudUseCases(); // Wordt fysiek gegenereerd
+//builder.Services.AddAutoApiServices(); // niet toegankelijk, want staat in gAPI.AutoApi.Server, die hem normaal genereerd
+builder.Services.AddScoped<IMailMessagesCrudService, MailMessagesCrudService>();
 builder.Services.AddSingleton<IConsoleService, ConsoleService>();
 builder.Services.AddSingleton<ILlmClient, OllamaClient>();
 
